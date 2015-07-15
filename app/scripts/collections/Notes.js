228adb9069330afb6d6b2d4665488fb07ae2b850
Andrés González <andres@simbiotica.es>
2015-07-15 13:34:14 +0200

    regexp

--- app/scripts/collections/Notes.js
+++ app/scripts/collections/Notes.js
@@ -12,7 +12,16 @@ define([
     url: '//globalintegrity.cartodb.com/api/v2/sql',
 
     parse: function(data) {
-      return data.rows;
+      return _.map(data.rows, function(note) {
+        return {
+          aspectid: note.aspectid,
+          targetid: note.targetid,
+          notetitle1: note.notetitle1,
+          notedata1: note.notedata1.replace(/(<[\/]?[^>]+>)/ig, ' '),
+          notetitle2: note.notetitle2,
+          notedata2: note.notedata2.replace(/([\/]?[^>]+>)/ig, ' '),
+        }
+      });
     },
 
     getNotes: function(question, target, callback) {
